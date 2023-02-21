6a56b26f2711823d293db7ceef97ab744031a11d
Your Name <irfana.27@gmail.com>
2023-02-21 01:56:53 -0800

    updated sign up and register

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
 import './App.css';
+import { auth } from './firebase';
 import HomeScreen from './screens/HomeScreen';
 import Login from './screens/Login';
 
 function App() {
   const user=null;
+  useEffect(()=>{
+    const unsubscribe=auth.onAuthStateChanged((userAuth)=>{
+      if(userAuth){
+
+      }else{
+
+      }
+    })
+    return unsubscribe;
+  },[])
   return (
     <div className="app">
        <Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app"
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyDZ8lnWTO9A4MpgK_4DrLbO4IRXwSjXdKg",
     authDomain: "movieflix-553a8.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "112879695944",
     appId: "1:112879695944:web:82c5b6c392faa35524007d"
   };
+
+  const firebaseApp=firebase.initializeApp(firebaseConfig)
+  const db = firebaseApp.firestore()
+  const auth= firebase.auth()
+
+  export{auth}
+  export default db;
